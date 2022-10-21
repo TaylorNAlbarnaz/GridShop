c3335762c2d85895fd5313237f0eee74ee5d446d
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-21 11:42:01 -0300

    Item da sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,14 @@
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Sidebar() {
   return (
     <>
       <aside>
-        
+        <div className="aside__item">
+          <FontAwesomeIcon icon={faCircle} />
+          <span>Periféricos</span>
+        </div>
       </aside>
     </>
   );
