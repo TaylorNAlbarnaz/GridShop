253ae1847346aaabd738faf524f61f204ca03d50
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-22 13:26:49 -0300

    Adicionado grid de items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import {Header, Sidebar, SidebarItem} from './components/';
+import {Header, Sidebar, SidebarItem, Item, Items} from './components/';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header/>
-      <Sidebar>
-        <SidebarItem name='Periféricos' selected/>
-        <SidebarItem name='Processadores'/>
-        <SidebarItem name='Placas de Vídeo'/>
-        <SidebarItem name='Monitores'/>
-      </Sidebar>
+      <section className='AppBody'>
+        <Sidebar>
+          <SidebarItem name='Periféricos' selected/>
+          <SidebarItem name='Processadores'/>
+          <SidebarItem name='Placas de Vídeo'/>
+          <SidebarItem name='Monitores'/>
+        </Sidebar>
+        <div className='Content'>
+          <Items>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+            <Item/>
+          </Items>
+
+          
+        </div>
+      </section>
     </div>
   );
 }
