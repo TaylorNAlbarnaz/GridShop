e2826c7e72d4253b21070c7fc741c6fd277d2abd
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-22 13:45:53 -0300

    Items de compra adicionados

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,16 @@ function Item() {
   return (
     <>
       <div className='Items__single'>
-        
+        <img
+          src="https://www.pngmart.com/files/16/Neon-Gaming-Keyboard-PNG-Photos.png"
+          alt='Imagem do produto'
+          width='80%'
+        />
+
+        <div className='Item__info'>
+          <p className='Item__price'>R$ 600,00</p>
+          <p className='Item__installments'>ou 12x de R$ 50,00 sem juros</p>
+        </div>
       </div>
     </>
   );
