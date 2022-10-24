15e3e9d4ddb7e422563baa1d7d9a6d7463c66e64
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-24 20:54:24 -0300

    Adicionado troca de seleção na sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import SidebarItem from "./SidebarItem";
 
 function Sidebar(props) {
+  const [current, setCurrent] = useState(0);
+
+  function changeSelected(id) {
+    setCurrent(id)
+    const items = document.getElementsByClassName('aside__item');
+  }
   return (
     <aside>
-      <SidebarItem onClick={() => console.log("Pika")} name='Periféricos' selected/>
-      <SidebarItem name='Processadores'/>
-      <SidebarItem name='Placas de Vídeo'/>
-      <SidebarItem name='Monitores'/>
+      <SidebarItem id={0} onClick={()=>changeSelected(0)} name='Periféricos' selected={current}/>
+      <SidebarItem id={1} onClick={()=>changeSelected(1)} name='Processadores' selected={current}/>
+      <SidebarItem id={2} onClick={()=>changeSelected(2)} name='Placas de Vídeo' selected={current}/>
+      <SidebarItem id={3} onClick={()=>changeSelected(3)} name='Monitores' selected={current}/>
     </aside>
   );
 }
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -5,9 +5,10 @@ function SidebarItem(props) {
   return (
     <div
       className = "aside__item"
-      style = {props.selected && {backgroundColor: '#524F4F', color: 'white'}}
+      style = {props.selected !== props.id ? {} : {backgroundColor: '#524F4F', color: 'white'}}
+      onClick = {props.onClick}
     >
-      <FontAwesomeIcon icon={faCircle} style={props.selected ? {} : {color: '#595959'}}/>
+      <FontAwesomeIcon icon={faCircle} style={props.selected == props.id ? {} : {color: '#595959'}}/>
       <p>{props.name}</p>
       <div className="aside__overlay" />
     </div>
