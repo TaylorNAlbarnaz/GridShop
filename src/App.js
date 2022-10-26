31db97cd7be2ea60efe33e13fdd52495d72a7dc6
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-10-26 11:21:52 -0300

    Sistema de mudança de página

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import {Header, Sidebar, Items, PageHandler} from './components/';
+import { Perifericos1, Perifericos2} from './components/item_pages';
 import './App.css';
 
 function App() {
+  const content = [<Perifericos1/>, <Perifericos2/>]
+
   return (
     <div className='App'>
       <Header/>
       <section className='AppBody'>
         <Sidebar />
         <div className='Content'>
-          <Items />
+          <Items category={content} page='0'/>
           <PageHandler />
         </div>
       </section>
---
+++ src/components/item_pages/index.js
@@ -0,0 +1,2 @@
+export {default as Perifericos1} from './Perifericos1';
+export {default as Perifericos2} from './Perifericos2';
